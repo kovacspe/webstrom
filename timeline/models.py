52c78fa9512b1ff8c699f62dcb1f2d1de2b31b8c
kovacspe <pet.kovacs1@gmail.com>
2020-06-03 12:13:20 +0200

    Prvý nástrel  modelov appky timeline

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from competition.models import Event
 
-# Create your models here.
+
+class TimelineEventType(models.Model):
+    class Meta:
+        verbose_name = 'Typ udalosti na časovej osi'
+        verbose_name_plural = 'Typy udalostí na časovej osi'
+        ordering = ['series', 'order', ]
+    name = models.CharField(
+        max_length=50,
+        verbose_name='názov'
+    )
+    icon = models.ImageField(verbose_name='ikona')
+    default_text = models.TextField(verbose_name='vzor textu')
+
+
+class TimelineEvent(models.Model):
+    class Meta:
+        verbose_name = 'Udalosť na časovej osi'
+        verbose_name_plural = 'Udalosti na časovej osi'
+        ordering = ['series', 'order', ]
+
+    event = models.ForeignKey(
+        Event, null=True, on_delete=models.CASCADE)
+    type = models.ForeignKey(TimelineEventType, verbose_name='typ udalosti')
+    show_after = models.DateTimeField(verbose_name='dátum a čas zverejnenia')
+    additional_info = models.TextField(verbose_name='doplňujúci text')
